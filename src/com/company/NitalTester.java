47b1bc8556d04158540dd42f8f925a94d2f0472f
Raghav Vaid <raghavvaid@billericak12.com>
2022-02-10 11:53:25 -0500

    asdf

--- src/com/company/NitalTester.java
+++ src/com/company/NitalTester.java
@@ -4,10 +4,6 @@ import java.util.Arrays;
 
 public class NitalTester {
 
-    /** Returns true if and only if every value in arr1 appears in arr2.
-     * Precondition: arr1 and arr2 are the same length.
-     * Postcondition: arr1 and arr2 are unchanged.
-     */
     public static boolean hasAllValues(int [] arr1, int[] arr2){
 
         for(int i = 0; i < arr1.length; i++){
@@ -20,8 +16,7 @@ public class NitalTester {
         return true;
     }
 
-    /** Returns true if arr contains any repeated values, false otherwise.
-     */
+
     public static boolean containsRepeats(int[]arr){
         for(int i = 0; i < arr.length-1; i++){
             for(int j = i+1; j < arr.length; j++){
@@ -37,25 +32,15 @@ public class NitalTester {
             result[i] = arr2D[i][c];
             }
         return result;
-
     }
 
-    /* Write the method isNital below.  You must use getColumn,
-    hasAllValues and containsRepeats appropriately to receive
-    full credit.
-     */
-    /** Returns true if square is a Nital Square as described in the
-     * word doc, false otherwise.
-     * Precondition: square has an equal number of rows and columns.
-     *      square has at least one row.
-     */
     public static boolean isNital(int[][] square){
         if (containsRepeats(square[0])) return false;
         for (int i = 0; i < square.length; i++){
             if (!hasAllValues(square[0],square[i])) return false;
         }
         for (int i = 0; i < square[0].length; i++){
-            if (!hasAllValues(square[0],square(getColumn(i)))) return false;
+            if (!hasAllValues(getColumn(square,0),(getColumn(square,i)))) return false;
         }
 
         return true;
@@ -76,23 +61,13 @@ public class NitalTester {
         System.out.println(Arrays.toString(getColumn(n5,1)));
         System.out.println(Arrays.toString(getColumn(n6,0)));
 
-//        System.out.println(isNital(n1));
-//        System.out.println(isNital(n2));
-//        System.out.println(isNital(n3));
-//        System.out.println(isNital(n4));
-//        System.out.println(isNital(n5));
-//        System.out.println(isNital(n6));
+        System.out.println(isNital(n1));
+        System.out.println(isNital(n2));
+        System.out.println(isNital(n3));
+        System.out.println(isNital(n4));
+        System.out.println(isNital(n5));
+        System.out.println(isNital(n6));
 
 
-//        int [] arr1 = {1,2,3};
-//        int [] arr2 = {3,1,2};
-//        int [] arr3 = {2,1,2};
-//        System.out.println(hasAllValues(arr1,arr2));
-//        System.out.println(hasAllValues(arr1,arr3));
-//        System.out.println(hasAllValues(arr3,arr2));
-//        int[] arr4 = {1,2,3,4,5,6};
-//        int[] arr5 = {3,2,1,2,3};
-//        System.out.println(containsRepeats(arr4));
-//        System.out.println(containsRepeats(arr5));
     }
 }
